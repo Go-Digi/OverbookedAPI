db02046ecb478149fc6fca888cdaed81d11a9809
nesdesa <141119897+nesdesa@users.noreply.github.com>
2025-02-26 13:34:11 +0800

    Finish Room Type Crud

--- Controllers/RoomTypesController.cs
+++ Controllers/RoomTypesController.cs
@@ -73,6 +73,60 @@ public class RoomTypesController : ControllerBase
         return CreatedAtAction(nameof(GetRoomType), new { id = roomType.RoomTypeId }, roomType);
     }
     
+    [HttpPut]
+    public async Task<ActionResult<RoomType>> UpdateRoomType(RoomTypeDTO newRoomType)
+    {
+        if (newRoomType.RoomTypeId == null || newRoomType.RoomTypeId == Guid.Empty)
+        {
+            return BadRequest("Room type id cannot be empty");
+        }
+        var roomType = await _context.RoomTypes.FirstOrDefaultAsync(rt => rt.RoomTypeId == newRoomType.RoomTypeId);
+        if (roomType == null)
+        {
+            return NotFound("Room type not found");
+        }
+        roomType.RoomTypeName = newRoomType.RoomTypeName;
+        roomType.RoomCount = newRoomType.RoomCount;
+        roomType.MaxCapacity = newRoomType.MaxCapacity;
+        roomType.HotelId = newRoomType.HotelId;
+        _context.RoomTypes.Update(roomType);
+
+        // since we only use one room rate for now, we don't need to list all rates
+        var roomRate = await _context.RoomRates.FirstOrDefaultAsync(rt => rt.RoomTypeId == newRoomType.RoomTypeId);
+        if (roomRate == null)
+        {
+            return NotFound("Room rate not found");
+        }
+        roomRate.Price = newRoomType.Price;
+        _context.RoomRates.Update(roomRate);
+        
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<RoomType>> DeleteRoomType(Guid id)
+    {
+        var roomType = await _context.RoomTypes.Include(rt => rt.RoomRates).FirstOrDefaultAsync(rt => rt.RoomTypeId == id);
+        if (roomType == null)
+        {
+            return NotFound();
+        }
+        _context.RoomTypes.Remove(roomType);
+        
+        var roomRate = await _context.RoomRates.FirstOrDefaultAsync(rt => rt.RoomTypeId == id);
+        if (roomRate == null)
+        {
+            return NotFound("Room rate not found");
+        }
+        _context.RoomRates.Remove(roomRate);
+        
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+    
     
 
     
