ee25300e0f807f82e93e087f537d8f2080f4369b
nesdesa <141119897+nesdesa@users.noreply.github.com>
2025-04-18 11:36:31 +0800

    Add authorization in endpoints

--- Controllers/RoomTypesController.cs
+++ Controllers/RoomTypesController.cs
@@ -4,12 +4,14 @@ using Overbookedapi.Models;
 using Overbookedapi.Models.DTO;
 using Overbookedapi.Data;
 using Overbookedapi.Utils;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Overbookedapi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class RoomTypesController : ControllerBase
+[Authorize]
+public class RoomTypesController : BaseApiController
 {
     private readonly DataSet _context;
     private readonly IUserActivityLogger _userActivityLogger;
--- Models/DTO/LoginRequest.cs
+++ Models/DTO/LoginRequest.cs
@@ -7,8 +7,5 @@ namespace Overbookedapi.Models.DTO
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
-        [Required]
-        public string Password { get; set; } = string.Empty;
     }
 } 
